288c3363e681323f4dd7a9212d15b04a63e8b21a
ArchangelSovereign-REIGN <leoannjireh@gmail.com>
2026-04-28 22:05:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function showJoinMessage() {
-alert("Welcome to Archangel Sovereigns! Message us to join.");
+function joinDiscord() {
+    window.open("https://discord.gg/https://discord.gg/3v23eFj95", "_blank");
 }
 
 document.querySelectorAll('.join-btn').forEach(button => {
-button.addEventListener('click', showJoinMessage);
+    button.addEventListener('click', joinDiscord);
 });
