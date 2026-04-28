4c044fbf4f0e23ffb2dcad34d2836e706b2f340d
ArchangelSovereign-REIGN <leoannjireh@gmail.com>
2026-04-28 20:53:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function showJoinMessage() {
+alert("Welcome to Archangel Sovereigns! Message us to join.");
+}
 
+document.querySelectorAll('.join-btn').forEach(button => {
+button.addEventListener('click', showJoinMessage);
+});
